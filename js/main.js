12e6817c8279999fa85588cd02aa6a17f387f827
Letalandroid <carlossoncra@gmail.com>
2023-01-16 20:37:20 -0500

    Add js/year.js from update this

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 import { countdown } from "./timeRestant.js";
+import { year } from "./year.js";
 
-countdown("Jan 01 2024 00:00:00", "time", "Feliz Año Nuevo!!");
+countdown(`Jan 01 ${ year } 00:00:00`, "time", "Feliz Año Nuevo!!");
+
+document.querySelector('#year').innerHTML = year;
